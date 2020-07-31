83ad411de669e31292935bbbda32510bcd35a07b
Kevin Montero <kevinwilding1997@gmail.com>
2020-07-30 23:22:51 -0400

    Footer component done

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../style/Footer.sass'
+const Footer = () => {
+    return (
+        <footer>
+            <p>WorkSmart All Right Reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
