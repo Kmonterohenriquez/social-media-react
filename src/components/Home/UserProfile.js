5cec1d5f63126e7fc336488ae4b29dc44f70182b
Kevin Montero <kevinwilding1997@gmail.com>
2020-07-30 23:25:57 -0400

    UserProfile component created

---
+++ src/components/Home/UserProfile.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import userPic from '../../img/user_pic.jpg'
+import '../../style/Home/UserProfile.sass'
+
+const UserProfile = () => {
+    return (
+        <div className='UserProfile' >
+            <div className="box-color"></div>
+            <img src={userPic} alt="" className='user-picture'/>
+            <h1 className="username">Kevin Montero</h1>
+            <p className="job-position">React Developer at Splay7</p>
+            <hr />
+            <p className="following">Following <span>13</span></p>
+            <hr />
+            <p className="followers">Followers <span>131</span></p>
+            <hr />
+            <p className="view-profile-btn">View Profile</p>
+        </div>
+    )
+}
+
+export default UserProfile
