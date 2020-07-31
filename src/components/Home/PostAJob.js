297bb7eff6ff56ee1dd1603a3451effc3766cfa9
Kevin Montero <kevinwilding1997@gmail.com>
2020-07-30 23:25:16 -0400

    PostAJob component created

---
+++ src/components/Home/PostAJob.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../../style/Home/PostAJob.sass'
+import user_pic from '../../img/user_pic.jpg'
+const PostAJob = () => {
+    return (
+        <div className='PostAJob'>
+            <div className="left-side">
+                <img src={user_pic} alt="" className="user-picture"/>
+            </div>
+            <div className="right-side">
+                <button className='secondary-btn btn'>Post a Project</button>
+                <button className='primary-btn btn'>Post a Job</button>
+            </div>
+        </div>
+    )
+}
+
+export default PostAJob
