72d8e36cc53b05aa126d93cff625b5857877c2a4
Kevin Montero <kevinwilding1997@gmail.com>
2020-08-01 03:12:22 -0400

    Menu component done

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import '../../style/Home/Menu.sass'
 
 const Menu = () => {
-    return (
-        <div className='Menu'>
-            
-        </div>
-    )
-}
+  return (
+    <div className="Menu">
+      <div className='options'>
+          <p>Help Center</p>
+          <p>About</p>
+          <p>Privacy Policy</p>
+          <p>Community Guidelines</p>
+          <p>Cookies Policy</p>
+          <p>Career</p>
+          <p>Language</p>
+      </div>
+      <div className="bottom">
+        <i className="fab fa-weebly logo"></i>
+        <p><i className="far fa-copyright"></i>Copyright 2020</p>
+      </div>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
