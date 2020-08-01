10f45fb1f04d15c21650b8b6a52cfdf96bb961cf
Kevin Montero <kevinwilding1997@gmail.com>
2020-08-01 03:15:20 -0400

    TopJob component created

---
+++ src/components/Home/TopJob.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../style/Home/TopJob.sass'
+const TopJob = () => {
+    return (
+        <div className='TopJob'>
+            <div className='TopJob-top'>
+                <p className="job-title">Senior Product Designer</p>
+                <p className="pay-rate">$35/hr</p>
+            </div>
+            <p className="job-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            </p>
+        </div>
+    )
+}
+
+export default TopJob
