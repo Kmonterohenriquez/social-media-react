1b222cc6fda3b8ea35b01c36cfe5c3b2dfbfdd93
Kevin Montero <kevinwilding1997@gmail.com>
2020-08-05 12:36:36 -0400

    more functionality added on footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import '../style/Footer.sass'
+import React from "react";
+import "../style/Footer.sass";
 const Footer = () => {
-    return (
-        <footer>
-            <p>WorkSmart <i className="far fa-copyright"></i> All Right Reserved</p>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <p>Help</p> <p>Center</p> <p>About</p> <p>Privacy Policy</p>{" "}
+      <p>Community Guidelines</p> <p>Cookies</p> <p>Policy</p> <p>Career</p>{" "}
+      <p>Forum</p> <p>Language</p> <p>Copyright</p> <p>Policy</p>
+      <p>
+        WorkSmart <i className="far fa-copyright"></i> All Right Reserved
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
