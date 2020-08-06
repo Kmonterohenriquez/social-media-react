e4b2ff45674e2188f7756fac64ac606b54119e0b
Kevin Montero <kevinwilding1997@gmail.com>
2020-08-05 21:14:15 -0400

    New packages imported to use MongoDB in the backend

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 require("dotenv").config();
 const express = require("express");
-const authCtrl = require("./controllers/AuthController");
-const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { SERVER_PORT, ATLAS_URI } = process.env;
 const app = express();
 
+const AuthRoute = require('./routes/AuthRoute')
+// Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.static(`${__dirname}/../build`));
 
+// MongoDB conection
+const uri = ATLAS_URI; 
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully!");
+});
+
 const port = SERVER_PORT;
+
+//Routes 
+app.use('/api', AuthRoute)
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
