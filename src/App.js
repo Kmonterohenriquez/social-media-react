60c5fc5103c189b050a37a3659a7b156f7a400ce
Kevin Montero <kevinwilding1997@gmail.com>
2020-08-05 21:08:03 -0400

    Container implemented on App.js file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./style/App.sass";
+
 // ROUTES
-import routes from "./router"
+import routes from "./router";
 
 // COMPONENTS
 import Nav from "./components/Nav";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
-import {withRouter} from "react-router-dom"
+import { withRouter } from "react-router-dom";
 
 function App(props) {
   let currLocation = props.location.pathname;
   return (
     <div className="App">
-      {currLocation !== '/user-auth'? <Nav />: null}
-      {routes}
-      <Footer/>
+      <div className="md-container">
+        {currLocation !== "/user-auth" ? <Nav /> : null}
+        {routes}
+        <Footer />
+      </div>
     </div>
   );
 }
