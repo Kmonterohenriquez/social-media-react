1699c880c1ae00069ea23264af9bd5c71f9c9131
Kevin Montero <kevinwilding1997@gmail.com>
2020-08-07 00:33:27 -0400

    New fields added and Register component is done

--- src/components/UserAuth/Signup.js
+++ src/components/UserAuth/Signup.js
@@ -1,26 +1,77 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "../../style/UserAuth/Signup.sass";
 import "../../style/UserAuth/Login.sass";
+import Axios from "axios";
 
-const Signup = () => {
+const Signup = ({ changeHandler }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const register = () => {
+    Axios.post("/api/register", {
+      username,
+      email,
+      password,
+      firstName,
+      lastName,
+    })
+      .then((res) => {
+        console.log(res);
+        changeHandler();
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="Login">
       <h1 className="Login-title">Sign up</h1>
       <div className="input-box">
         <i className="fas fa-user"></i>
-        <input type="text" placeholder="Username" />
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="input-box">
+        <i className="fas fa-user"></i>
+        <input
+          type="text"
+          placeholder="First name"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+      <div className="input-box">
+        <i className="fas fa-user"></i>
+        <input
+          type="text"
+          placeholder="Last Name"
+          onChange={(e) => setLastName(e.target.value)}
+        />
       </div>
       <div className="input-box">
         <i className="fas fa-envelope"></i>
-        <input type="text" placeholder="Email" />
+        <input
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="input-box">
         <i className="fas fa-unlock-alt"></i>
-        <input type="text" placeholder="Password" />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
 
-      <button className="primary-btn ">Register</button>
+      <button className="primary-btn" onClick={() => register()}>
+        Register
+      </button>
       <div className="bottom">
         <h1>Signup via social account</h1>
         <div className="social-media">
