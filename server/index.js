e17a2ca590c77a5da3b93f6f604919c04ec4b4e7
Kevin Montero <kevinwilding1997@gmail.com>
2020-09-12 10:44:33 -0400

    post route added

--- server/index.js
+++ server/index.js
@@ -5,15 +5,21 @@ const mongoose = require("mongoose");
 const { SERVER_PORT, ATLAS_URI } = process.env;
 const app = express();
 
-const AuthRoute = require('./routes/AuthRoute')
+const AuthRoute = require("./routes/AuthRoute");
+const PostRoute = require("./routes/PostRoute");
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.static(`${__dirname}/../build`));
 
 // MongoDB conection
-const uri = ATLAS_URI; 
-mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+const uri = ATLAS_URI;
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully!");
@@ -21,7 +27,8 @@ connection.once("open", () => {
 
 const port = SERVER_PORT;
 
-//Routes 
-app.use('/api', AuthRoute)
+//Routes
+app.use("/api", AuthRoute);
+app.use("/posts", PostRoute);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
