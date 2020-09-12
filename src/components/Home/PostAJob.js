fef040823f80f8092db91ac10d5dd0bea7316e3c
Kevin Montero <kevinwilding1997@gmail.com>
2020-09-12 10:46:14 -0400

    PostAJob component created

--- src/components/Home/PostAJob.js
+++ src/components/Home/PostAJob.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import '../../style/Home/PostAJob.sass'
-import user_pic from '../../img/user_pic.jpg'
-const PostAJob = () => {
-    return (
-        <div className='PostAJob'>
-            <div className="left-side">
-                <img src={user_pic} alt="" className="user-picture"/>
-            </div>
-            <div className="right-side">
-                <button className='secondary-btn btn'>Post a Project</button>
-                <button className='primary-btn btn'>Post a Job</button>
-            </div>
-        </div>
-    )
-}
+import React, { useEffect } from "react";
+import "../../style/Home/PostAJob.sass";
+import user_pic from "../../img/user_pic.jpg";
+import Axios from "axios";
+const PostAJob = ({ modalHandler }) => {
+  useEffect(() => {
+    Axios.post("/posts", {})
+      .then((res) => console.log("Post created successfully!"))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="PostAJob">
+      <div className="left-side">
+        <img src={user_pic} alt="" className="user-picture" />
+      </div>
+      <div className="right-side">
+        <button className="secondary-btn btn">Post a Project</button>
+        <button className="primary-btn btn" onClick={() => modalHandler()}>
+          Post a Job
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default PostAJob
+export default PostAJob;
