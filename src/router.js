89220608f6aad80df4b6ef3da024a6c3db1a537e
Kevin Montero <kevinwilding1997@gmail.com>
2020-09-14 02:56:01 -0400

    Companies route added

--- src/router.js
+++ src/router.js
@@ -4,10 +4,12 @@ import { Switch, Route } from "react-router-dom";
 // PAGES
 import Home from "./Views/Home";
 import UserAuth from "./Views/UserAuth";
+import Companies  from "./Views/Companies"
 
 export default (
   <Switch>
     <Route component={Home} exact path="/dashboard" />
     <Route component={UserAuth} exact path="/" />
+    <Route component={Companies} exact path="/companies"/>
   </Switch>
 );
