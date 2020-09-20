0a50c558f010486e06deb86131230d277826dd2f
Kevin Montero <kevinwilding1997@gmail.com>
2020-09-20 19:07:15 -0400

    Company component created for company view

---
+++ src/components/Companies/Company.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Company = ({ CompanyInfo }) => {
+  //   const {
+  const name = "KWMH";
+  const logo = "https://kevinmontero.com/static/media/my_logo.e70af071.png";
+  const founded = "Feb 04";
+
+  // } = CompanyInfo;
+  return (
+    <div className="Company">
+      <img src={logo} alt="name" />
+      <h1>{name}</h1>
+      <p>Establish {founded}</p>
+      <div className='btn-container'>
+        <button className="follow-btn">Follow</button>
+        <button className="msg-btn">
+          <i className="fas fa-envelope"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Company;
