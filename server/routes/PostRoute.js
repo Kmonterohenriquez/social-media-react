82034f8129294ad4a8a6f4f381e858a0bcdd1ee9
Kevin Montero <kevinwilding1997@gmail.com>
2020-09-20 19:05:26 -0400

    New routes added for Post manipulation

--- server/routes/PostRoute.js
+++ server/routes/PostRoute.js
@@ -6,7 +6,7 @@ const PostController = require('../controllers/PostController')
 router.get("/", PostController.getPosts); // GET ALL POSTS
 router.get("/:post_id", PostController.getOnePost) // GET ONE POST
 router.post('/', PostController.createPost) // CREATE POST
-router.put('/:post_id') // UPDATE POST
+router.put('/:post_id', PostController.updatePost) // UPDATE POST
 router.delete("/:post_id", PostController.deletePost); // DELETE POST
 
 module.exports = router;
