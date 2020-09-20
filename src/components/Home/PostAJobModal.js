26dad072b2077d837fcbdf819fec7870d09d3d85
Kevin Montero <kevinwilding1997@gmail.com>
2020-09-20 19:09:26 -0400

    PostAJobModal updated with new input field to upload images

--- src/components/Home/PostAJobModal.js
+++ src/components/Home/PostAJobModal.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "../../style/Home/PostAJobModal.sass";
 
-function PostAJobModal({ modalHandler }) {
+function PostAJobModal({ modalHandler, getPosts }) {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [skills, setSkills] = useState("");
+  const [category, setCategory] = useState("");
+  const [salary, setSalary] = useState("");
+  const [description, setDescription] = useState("");
+
+  function createPost() {
+    axios
+      .post("/posts", { title, image, skills, salary, category, description })
+      .then((res) => {
+        modalHandler();
+        getPosts();
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }
   return (
     <div className="PostAJobModal">
       <div className="header-container">
@@ -12,21 +30,49 @@ function PostAJobModal({ modalHandler }) {
         ></i>
       </div>
       <div className="inputs-container">
-        <input type="text" placeholder="Title" />
+        <input
+          type="text"
+          placeholder="Title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Image"
+          onChange={(e) => setImage(e.target.value)}
+        />
         <div className="input-box">
-          <select name="" id="category" placeholder="category">
+          <select
+            name=""
+            id="category"
+            placeholder="category"
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <option value="">Choose category</option>
-            <option value="">Remote</option>
-            <option value="">Full-time</option>
-            <option value="">Contract</option>
-            <option value="">Contract to Hire</option>
-            <option value="">Temporary</option>
+            <option value="remote">Remote</option>
+            <option value="full-time">Full-time</option>
+            <option value="contract">Contract</option>
+            <option value="contract to hire">Contract to Hire</option>
+            <option value="temporary">Temporary</option>
           </select>
-          <input type="text" placeholder="Salary" />
+          <input
+            type="text"
+            placeholder="Salary"
+            onChange={(e) => setSalary(e.target.value)}
+          />
         </div>
-        <input type="text" placeholder="Skills" />
-        <textarea placeholder="Brief description..." rows="5"></textarea>
-        <button className="btn ">Post</button>
+        <input
+          type="text"
+          placeholder="Skills"
+          onChange={(e) => setSkills(e.target.value)}
+        />
+        <textarea
+          placeholder="Brief description..."
+          rows="5"
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+        <button className="btn " onClick={() => createPost()}>
+          Post
+        </button>
       </div>
     </div>
   );
