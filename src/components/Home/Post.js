6c164e47d31fd3beb16c1bf209e755b70e15767f
Kevin Montero <kevinwilding1997@gmail.com>
2020-09-20 19:08:32 -0400

    Post component updaded with new features like type of job and its is dynamic now

--- src/components/Home/Post.js
+++ src/components/Home/Post.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import userPic from "../../img/user_pic.jpg";
 import "../../style/Home/Post.sass";
+import PostComment from "./PostComment";
+import axios from "axios";
 
-const Post = ({ post }) => {
+const Post = ({ post, getPosts }) => {
+  const [postMenu, setPostMenu] = useState(false);
+  const deletePost = async (id) => {
+    await axios
+      .delete(`/posts/${id}`)
+      .then(async (res) => {
+        await getPosts();
+        setPostMenu(false);
+      })
+      .catch((err) => console.log("droga"));
+  };
   return (
     <div className="Post">
       <div className="top-part">
@@ -15,45 +27,59 @@ const Post = ({ post }) => {
             </p>
           </div>
         </div>
-        <i className="fas fa-ellipsis-v menu-icon"></i>
+        <i
+          className="fas fa-ellipsis-v menu-icon"
+          onClick={() => setPostMenu(!postMenu)}
+        ></i>
+        {postMenu ? (
+          <div className="post-menu">
+            <ul>
+              {/* <li>
+                <div>
+                  <i className="far fa-bookmark"></i>
+                </div>{" "}
+                Save post
+              </li> */}
+              {/* <li>
+                <div>
+                  <i className="far fa-eye-slash"></i>
+                </div>{" "}
+                Hide from
+              </li> */}
+              <li onClick={() => deletePost(post._id)}>
+                <div>
+                  <i className="far fa-trash-alt"></i>
+                </div>{" "}
+                Remove
+              </li>
+            </ul>
+          </div>
+        ) : null}
       </div>
       <div className="description">
         <div className="description-top-part">
           <p className="title">{post.title}</p>
           <div>
-            <p className="job-type">{post.type}</p>
+            <p className="job-type">{post.category}</p>
             <p className="pay-rate">${post.salary}</p>
           </div>
         </div>
         <p className="description-bottom-part">{post.description}</p>
-      </div>
-      <div className="tech-used">
-        <p>HTML</p>
-        <p>Sass</p>
-        <p>React</p>
-        <p>Node</p>
-        <p>Sass</p>
-      </div>
-      <hr />
-      <div className="comment-section">
-        <p className="like-btn">
-          <i className="far fa-thumbs-up"></i> Like
-        </p>
-        <p className="comment-btn">
-          <i className="far fa-comment-alt"></i> Comment
-        </p>
-        <p className="share-btn">
-          <i className="fas fa-share"></i> Share
-        </p>
-      </div>
-      <hr />
-      <div className="submit-commet-bar">
-        <img src={userPic} alt="" />
-        <div className="input-container">
-          <input type="text" placeholder="Write a comment..." />
-          <i className="far fa-smile"></i>
+        <div className="tech-used">
+          <p>HTML</p>
+          <p>Sass</p>
+          <p>React</p>
+          <p>Node</p>
+          <p>Sass</p>
         </div>
+        {post.image ? (
+          <div className="img-container">
+            <img src={post.image} className="post-img" alt="" />
+          </div>
+        ) : null}
       </div>
+      <hr />
+      <PostComment />
     </div>
   );
 };
