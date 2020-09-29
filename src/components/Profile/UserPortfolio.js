14d9264416c4950abf289609abf6e001bc1ab7e8
Kevin Montero <kevinwilding1997@gmail.com>
2020-09-28 23:59:59 -0400

    UserPortfolio component created

---
+++ src/components/Profile/UserPortfolio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserPortfolio = () => {
+    return (
+        <div>
+            <h1>User Portfolio component</h1>
+        </div>
+    )
+}
+
+export default UserPortfolio
