0380bb54cb75bf2aa1f0c93d5241de084e916726
Kevin Montero <kevinwilding1997@gmail.com>
2020-09-30 23:56:36 -0400

    UserInfo component created

--- src/components/Profile/UserInfo.js
+++ src/components/Profile/UserInfo.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import '../../style/Profile/UserInfo.sass'
 
 const UserInfo = () => {
     return (
-        <div className='UserInfo'>
-            <div className="UserInfo-container">
-                <div className="section about"><i className="fas fa-pen"></i><h1 className="title">About</h1><p className="info">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, at explicabo vero expedita quibusdam necessitatibus? Suscipit corrupti nisi voluptate beatae nam libero, ipsam, exercitationem, eligendi accusantium et perferendis quidem? Et.</p></div>
-                <div className="section websites"><i className="fas fa-pen"></i><h1 className="title">Websites</h1><p className="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi atque delectus neque minima magnam nostrum aut placeat sunt, provident ducimus pariatur itaque illum dolores iste sit recusandae suscipit quas necessitatibus.</p></div>
-                
-            </div>
+        <div className="UserInfo">
+            <h1>User Info Component</h1>
         </div>
     )
 }
