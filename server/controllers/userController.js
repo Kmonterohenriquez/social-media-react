acdf5f706796c033e326aca39998653d2321ebaf
Kevin Montero <kevinwilding1997@gmail.com>
2020-10-07 11:15:51 -0400

    user  controller created and user current method added

---
+++ server/controllers/userController.js
@@ -0,0 +1,26 @@
+const User = require("../models/user.model");
+
+const currUserInfo = (req, res, next) => {
+  const { email } = req.params;
+  User.find({ $or: [{ email: email }] })
+    .then((user) => {
+      if (user) {
+        res.status(200).json({
+          status: "succes",
+          message: "User Found!",
+          data: user,
+        });
+        return;
+      } else {
+        res.status(401).json({
+          status: "error",
+          message: "User not found.",
+          data: user,
+        });
+        return;
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = { currUserInfo };
