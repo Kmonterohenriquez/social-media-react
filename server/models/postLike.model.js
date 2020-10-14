e87b3f5e50a844d4eb97f7296ad2d9a8e05beb1e
Kevin Montero <kevinwilding1997@gmail.com>
2020-10-14 19:13:03 -0400

    model created and still working on it

---
+++ server/models/postLike.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const postLikeSchema = new Schema({
+  postID: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  userID: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  like: {
+    type: Boolean,
+    require: true,
+  },
+});
+
+const PostLike = mongoose.model("PostLike", postLikeSchema);
+
+module.exports = PostLike;
