13441daefe1f08984b9f4906e39f4c159841e921
Kevin Montero <kevinwilding1997@gmail.com>
2020-10-16 19:23:57 -0400

    basic structure added

---
+++ src/components/Chat/ChatRoom.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../style/Chat/ChatRoom.sass";
+
+const ChatRoom = () => {
+  return (
+    <div className="ChatRoom">
+      <div className="ChatRoom-header">
+        <i className="fas fa-times"></i>
+      </div>
+    </div>
+  );
+};
+
+export default ChatRoom;
