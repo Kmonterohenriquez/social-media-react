5d1e3d31f2793f24574e170281118a62cbc7ef6c
Kevin Montero <kevinwilding1997@gmail.com>
2020-10-16 19:29:30 -0400

    Component updated

--- src/components/Profile/HeaderProfile.js
+++ src/components/Profile/HeaderProfile.js
@@ -2,13 +2,15 @@ import React from "react";
 import me from "../../img/user_pic.jpg";
 import "../../style/Profile/HeaderProfile.sass";
 
-const HeaderProfile = ({ handleMenu }) => {
+const HeaderProfile = ({ handleMenu, currUser }) => {
+  const {firstName, lastName} = currUser
   return (
     <div className="HeaderProfile">
       <div className="profile-image-container">
-        <i class="fas fa-camera"></i>
+        <i className="fas fa-camera"></i>
         <img src={me} alt="" />
       </div>
+      <h1>{firstName} {lastName}</h1>
       <div className="HeaderProfile-current-job">React Developer</div>
       <div className="section-container">
         <p className="section" onClick={()=>handleMenu('feed')}><i className="far fa-newspaper"></i>Feed</p>
