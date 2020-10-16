8f77983af5675be429b9d041c7c5213671aba3af
Kevin Montero <kevinwilding1997@gmail.com>
2020-10-16 19:28:15 -0400

    Component updated

--- src/components/Home/PostAJob.js
+++ src/components/Home/PostAJob.js
@@ -1,13 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "../../style/Home/PostAJob.sass";
 import user_pic from "../../img/user_pic.jpg";
-import Axios from "axios";
 const PostAJob = ({ modalHandler }) => {
-  useEffect(() => {
-    Axios.post("/posts", {})
-      .then((res) => console.log("Post created successfully!"))
-      .catch((err) => console.log(err));
-  }, []);
   return (
     <div className="PostAJob">
       <div className="left-side">
