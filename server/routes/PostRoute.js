55500038b473ca0c13dc2ab60d15a7182b79fad1
Kevin Montero <kevinwilding1997@gmail.com>
2020-10-16 19:20:47 -0400

    Route created

--- server/routes/PostRoute.js
+++ server/routes/PostRoute.js
@@ -1,12 +1,13 @@
-const express = require ("express")
+const express = require("express");
 const router = express.Router();
 
-const PostController = require('../controllers/PostController')
+const PostController = require("../controllers/PostController");
 
 router.get("/", PostController.getPosts); // GET ALL POSTS
-router.get("/:post_id", PostController.getOnePost) // GET ONE POST
-router.post('/', PostController.createPost) // CREATE POST
-router.put('/:post_id', PostController.updatePost) // UPDATE POST
+router.get("/:post_id", PostController.getOnePost); // GET ONE POST
+router.get("/profile/:userID", PostController.getUserPosts); // GET USER'S POSTS
+router.post("/", PostController.createPost); // CREATE POST
+router.put("/:post_id", PostController.updatePost); // UPDATE POST
 router.delete("/:post_id", PostController.deletePost); // DELETE POST
 
 module.exports = router;
